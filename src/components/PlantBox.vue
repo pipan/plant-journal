55f90a936687df4ad877576cae8fff93485c8cae
Peter Gasparik <gasparik.pe@gmail.com>
2023-03-02 19:35:21 +0100

    show last seven events

--- src/components/PlantBox.vue
+++ src/components/PlantBox.vue
@@ -1,14 +1,18 @@
 <script setup>
 import { reactive } from "@vue/reactivity"
-import { computed } from "@vue/runtime-core"
+import { computed, onMounted, watch } from "@vue/runtime-core"
+import { useEventRepository } from "../repository/eventRepository"
 import { hslCss } from '../services/color'
+
+const eventRepository = useEventRepository()
 
 const props = defineProps({
     plant: Object
 })
 
 const data = reactive({
-    mouse: ''
+    mouse: '',
+    eventTypes: ['', '', '', '', '', '', '']
 })
 
 const emit = defineEmits(['move', 'select', 'selectHold'])
@@ -45,6 +49,38 @@ function selectHold() {
     emit('selectHold')
 }
 
+function loadEventTypes() {
+    eventRepository.selectByPot(props.plant.id).then((events) => {
+        const dayMs = 24 * 60 * 60 * 1000
+        let dateLimit = new Date()
+        dateLimit.setTime(dateLimit.getTime() - 6 * dayMs)
+        dateLimit.setHours(0, 0, 0, 0)
+        events = events.filter((e) => {
+            return e.createdAt >= dateLimit
+        })
+        events.sort((a, b) => {
+            if (a.createdAt.getTime() < b.createdAt.getTime()) return 1
+            else if (a.createdAt.getTime() > b.createdAt.getTime()) return -1
+            else return 0
+        })
+        for (let event of events) {
+            let index = 6 - Math.floor((event.createdAt.getTime() - dateLimit.getTime()) / dayMs)
+            if (data.eventTypes[index]) {
+                continue
+            }
+            data.eventTypes[index] = event.type
+        }
+    })
+}
+
+watch(() => props.plant, () => {
+    loadEventTypes()
+})
+
+onMounted(() => {
+    loadEventTypes()
+})
+
 </script>
 
 <template>
@@ -56,7 +92,9 @@ function selectHold() {
     @click.stop
     v-drag="move">
     <div class="column gap-s column--center" v-hold @contextmenu.prevent.stop="selectHold()">
-        <div class="plant" :class="'shape--' + plant.shape"></div>
+        <div class="plant" :class="'shape--' + plant.shape">
+            <div class="event" :class="eventType ? 'color-' + eventType : 'color--border'" v-for="(eventType, index) of data.eventTypes" :key="index"></div>
+        </div>
         <div class="plant-label" v-if="plant.name">{{ plant.name }}</div>
     </div>
 </div>
@@ -108,6 +146,7 @@ function selectHold() {
 }
 
 .plant {
+    position: relative;
     border: 3px solid var(--color-value);
     box-sizing: border-box;
     font-size: 20px;
@@ -130,4 +169,34 @@ function selectHold() {
 .holdable::before {
     top: -6px;
 }
+
+.volume--s .event { --event-size: 4px; }
+.volume--m .event { --event-size: 6px; }
+.volume--l .event { --event-size: 8px; }
+.event {
+    position: absolute;
+    width: var(--event-size);
+    height: var(--event-size);
+    left: 50%;
+    top: 50%;
+    background-color: var(--color-value);
+    border-radius: 100%;
+}
+
+.event:nth-child(7n+1) { transform: translateX(85%) translateY(-185%); }
+.event:nth-child(7n+2) { transform: translateX(150%) translateY(-50%); opacity: 0.92; }
+.event:nth-child(7n+3) { transform: translateX(85%) translateY(85%); opacity: 0.84; }
+.event:nth-child(7n+4) { transform: translateX(-50%) translateY(150%); opacity: 0.76; }
+.event:nth-child(7n+5) { transform: translateX(-185%) translateY(85%); opacity: 0.68; }
+.event:nth-child(7n+6) { transform: translateX(-250%) translateY(-50%); opacity: 0.6; }
+.event:nth-child(7n+7) { transform: translateX(-185%) translateY(-185%); opacity: 0.52; }
+
+.shape--rectangle .event:nth-child(7n+1) { transform: translateX(85%) translateY(-135%); }
+.shape--rectangle .event:nth-child(7n+2) { transform: translateX(200%) translateY(-50%); }
+.shape--rectangle .event:nth-child(7n+3) { transform: translateX(85%) translateY(35%); }
+.shape--rectangle .event:nth-child(7n+4) { transform: translateX(-50%) translateY(35%); }
+.shape--rectangle .event:nth-child(7n+5) { transform: translateX(-185%) translateY(35%); }
+.shape--rectangle .event:nth-child(7n+6) { transform: translateX(-300%) translateY(-50%); }
+.shape--rectangle .event:nth-child(7n+7) { transform: translateX(-185%) translateY(-135%); }
+
 </style>
